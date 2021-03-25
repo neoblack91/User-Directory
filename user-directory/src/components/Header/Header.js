e70ef3fd884ba7dac5ae70f3b4378edb3fcfe23c
Rajahni fields <rajahnifields@gmail.com>
2021-03-24 22:02:53 -0400

    organized file now working on api

---
+++ user-directory/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Style from "srcStylesStylesH.css";
+function Jumbotron() {
+  return (
+    <div className="jumbotron ">
+      <h1>Employee </h1>
+      {/* <Search /> */}
+    </div>
+  );
+}
+export default Jumbotron;
