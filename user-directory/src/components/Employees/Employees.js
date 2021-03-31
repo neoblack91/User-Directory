baf4906f3236bd2a9e461eb8bae270eebe1d3b74
Rajahni fields <rajahnifields@gmail.com>
2021-03-31 01:47:27 -0400

    fixing api call again

--- user-directory/src/components/Employees/Employees.js
+++ user-directory/src/components/Employees/Employees.js
@@ -1,88 +1,86 @@
 import React from "react";
-import { MDBDataTable } from "mdbreact";
 
-function DatatablePage(props) {
-  console.log(props);
+function employedPeople(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.employeeData.map(({ picture, name, phone, email }) => {
+          // console.log(picture, name, phone, email);
+          return (
+            <tr>
+              <img alt={name.first + "," + name.last} src={picture.medium} />
 
-  const data = {
-    columns: [
-      {
-        label: "Image",
-        field: "Image",
-        sort: "asc",
-        width: 150,
-      },
-      {
-        label: "First",
-        field: "first",
-        sort: "asc",
-        width: 150,
-      },
-      {
-        label: "Last",
-        field: "last",
-        sort: "asc",
-        width: 270,
-      },
-      {
-        label: "Phone",
-        field: "phone",
-        sort: "asc",
-        width: 200,
-      },
-      {
-        label: "Email",
-        field: "email",
-        sort: "asc",
-        width: 100,
-      },
-    ],
-    rows: [
-      {
-        image: [props.Image],
-        first: [props.first],
-        last: [props.last],
-        phone: [props.phone],
-        email: [props.email],
-      },
-    ],
-  };
-  return <MDBDataTable striped bordered small data={data} />;
+              <td>{name.first}</td>
+              <td>{name.last}</td>
+              <td>{phone}</td>
+              <td>{email}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
-// function employedPeople(props) {
-//   <table className="table">
-//     <thead>
-//       <tr>
-//         <th scope="col">id</th>
-//         <th scope="col">First</th>
-//         <th scope="col">Last</th>
-//         <th scope="col">Phone Number</th>
-//         <th scope="col">email</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       {props.employeeData.map(({ picture, name, phone, email }) => {
-//         // console.log(picture, name, phone, email);
-//         props.employeeData.map((item) =>
-//           item.type === "Internal" ? (
-//             <div className="internal">{item.title}</div>
-//           ) : (
-//             <div className="external">{item.title}</div>
-//           )
-//         );
-//         return (
-//           <tr>
-//             <img alt={name.first + "," + name.last} src={picture.medium} />
-//             <td>{name.first}</td>
-//             <td>{name.last}</td>
-//             <td>{phone}</td>
-//             <td>{email}</td>
-//           </tr>
-//         );
-//       })}
-//     </tbody>
-//   </table>;
+export default employedPeople;
+// import { MDBDataTable } from "mdbreact";
+
+// function DatatablePage(props) {
+//   console.log(props);
+
+//   const data = {
+//     columns: [
+//       {
+//         label: "Image",
+//         field: "Image",
+//         sort: "asc",
+//         width: 150,
+//       },
+//       {
+//         label: "First",
+//         field: "first",
+//         sort: "asc",
+//         width: 150,
+//       },
+//       {
+//         label: "Last",
+//         field: "last",
+//         sort: "asc",
+//         width: 270,
+//       },
+//       {
+//         label: "Phone",
+//         field: "phone",
+//         sort: "asc",
+//         width: 200,
+//       },
+//       {
+//         label: "Email",
+//         field: "email",
+//         sort: "asc",
+//         width: 100,
+//       },
+//     ],
+
+//     rows: [
+//       {
+//         image: [props.Image],
+//         first: [props.first],
+//         last: [props.last],
+//         phone: [props.phone],
+//         email: [props.email],
+//       },
+//     ],
+//   };
+//   return <MDBDataTable striped bordered small data={data} />;
 // }
-export default DatatablePage;
-// export default employedPeople;
+// export default DatatablePage;
